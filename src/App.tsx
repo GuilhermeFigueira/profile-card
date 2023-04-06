1b08f88945c7e5427bcdb27ab6dbef718050c4d9
Guilherme Figueira <guibf2004@gmail.com>
2023-04-06 12:20:46 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 function App() {
 	let imgName = "/image-victor.jpg";
+	let name = "Victor Crest";
+	let age = "26";
+	let city = "London";
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center background-images">
-			<main className="flex flex-col m-auto items-center bg-white">
-				<header>
-					<img src={imgName} alt="" />
-					<div className="flex flex-row">
-						<h1>Victor Crest</h1> <span>26</span>
+			<main className="flex flex-col m-auto items-center bg-white w-[22rem] h-[25rem] rounded-2xl shadow-2xl ">
+				<header className="flex flex-col w-full h-full justify-end items-center gap-6 pb-10 [background-image:url(/bg-pattern-card.svg)] bg-no-repeat bg-contain rounded-t-2xl">
+					<img
+						src={imgName}
+						alt=""
+						className="rounded-full border-[6px] border-white"
+					/>
+					<div className="flex flex-col items-center">
+						<div className="flex flex-row items-center gap-2 justify-center">
+							<h1 className="text-xl font-black text-blue-dark">
+								{name}
+							</h1>{" "}
+							<span className="text-[1.28rem] text-gray">
+								{age}
+							</span>
+						</div>
+						<h3 className="text-gray">{city}</h3>
 					</div>
-					<h3>London</h3>
 				</header>
-				<footer className="flex flex-row">
+				<footer className="flex flex-row justify-around w-full px-6 py-7 border-t-[1px] border-gray/60">
 					<section>
 						<h2>80K</h2>
 						<span>Followers</span>
