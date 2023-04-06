b591e98886b895cbddfed199b4c75a7f2b6c48b7
Guilherme Figueira <guibf2004@gmail.com>
2023-04-06 13:00:17 -0300

    agr finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
+
 function App() {
-	let imgName = "/image-victor.jpg";
-	let name = "Victor Crest";
-	let age = "26";
-	let city = "London";
+	const [guilherme, setGuilherme] = useState(false);
+	let imgName = [
+		"/image-victor.jpg",
+		"https://github.com/guilhermefigueira.png",
+	];
+	let name = ["Victor Crest", "Guilherme Figueira"];
+	let age = ["26", "18"];
+	let city = ["London", "São Paulo"];
+
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center background-images">
 			<main className="flex flex-col m-auto items-center bg-white w-[22rem] h-[25rem] rounded-2xl shadow-2xl ">
 				<header className="flex flex-col w-full h-full justify-end items-center gap-6 pb-10 [background-image:url(/bg-pattern-card.svg)] bg-no-repeat bg-contain rounded-t-2xl">
 					<img
-						src={imgName}
+						src={guilherme ? imgName[0] : imgName[1]}
 						alt=""
-						className="rounded-full border-[6px] border-white"
+						className="rounded-full border-[6px] border-white h-[6.5rem] w-[6.5rem]"
 					/>
 					<div className="flex flex-col items-center">
 						<div className="flex flex-row items-center gap-2 justify-center">
-							<h1 className="text-xl font-black text-blue-dark">
-								{name}
-							</h1>{" "}
+							<button onClick={() => setGuilherme(!guilherme)}>
+								<h1 className="text-xl font-black text-blue-dark underline">
+									{guilherme ? name[0] : name[1]}
+								</h1>{" "}
+							</button>
 							<span className="text-[1.28rem] text-gray">
-								{age}
+								{guilherme ? age[0] : age[1]}
 							</span>
 						</div>
-						<h3 className="text-gray">{city}</h3>
+						<h3 className="text-gray">
+							{guilherme ? city[0] : city[1]}
+						</h3>
 					</div>
 				</header>
 				<footer className="flex flex-row justify-around w-full px-6 py-7 border-t-[1px] border-gray/60">
